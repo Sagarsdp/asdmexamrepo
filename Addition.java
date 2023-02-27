5dadd68c6ae7bb470f6fbebf0aab4ceb7d1e499c
sagarsdp <mickey.chaudhary24278@gmail.com>
2023-02-27 11:54:57 +0530

    First Commit

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number 1");
+		
+		int num1 =sc.nextInt();
+		
+		System.out.println("Enter Number 2");
+		int num2 =sc.nextInt();
+		
+		System.out.println("Addition of num1 and num2 is "+ (num1+num2));
+		
+
+	}
+
+}
